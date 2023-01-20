9e25b709c1b7f5e6aa255e91174750f0f5dc1db2
ZahraRahimii <93929227+ZahraRahimii@users.noreply.github.com>
2023-01-20 12:08:43 +0330

    Complete task 3: implementation of the survivor selection algorithms

--- evolution.py
+++ evolution.py
@@ -1,5 +1,6 @@
 from player import Player
 import numpy as np
+from numpy import random
 from config import CONFIG
 
 
@@ -46,5 +47,50 @@ class Evolution():
 
         # TODO (additional): a selection method other than `top-k`
         # TODO (additional): plotting
+        
+        'Implementing top-k algorithm'
+        # selected_next_population = self.top_k_algorithm(players, num_players)
+        
+        'Implement roulette wheel'
+        # selected_next_population = self.rolette_wheel_algorithm(players)
 
-        return players[: num_players]
+        'Implement SUS'
+        selected_next_population = self.stochastic_universal_sampling_algorithm(players, num_players)
+
+        # Implement Q-tournament
+        return selected_next_population
+    
+    def top_k_algorithm(self, players, num_players):
+        sorted_players = sorted(players, key=lambda x: x.fitness, reverse=True)
+        top_k_players = sorted_players[:num_players]
+        return top_k_players
+    
+    def rolette_wheel_algorithm(self, players):
+        # Computes the totallity of the population fitness
+        players_fitness = sum([p.fitness for p in players])
+
+        # Computes for each chromosome the probability 
+        players_probs = [p.fitness/players_fitness for p in players]
+
+        return players[random.choice(len(players), p=players_probs)]
+
+    def stochastic_universal_sampling_algorithm(self, players, num_players):
+        selected_players = []
+        max_fitness = sum([p.fitness for p in players])
+        players_probs = [p.fitness/max_fitness for p in players]
+
+        step_size = 1.0/num_players
+        r = random.unifrom(0, step_size)
+
+        cumulative_prob = 0
+        for i in range(players):
+            players_prob = players_probs[i]
+            cumulative_prob += players_probs
+            if r <= cumulative_prob:
+                selected_players.append(players[i])
+                r += step_size
+        
+        if len(selected_players) < num_players:
+            selected_players.append(players[-1])
+
+        return selected_players
